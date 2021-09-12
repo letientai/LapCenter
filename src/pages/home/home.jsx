5a70879ed1a6154cf1b24929be3d6d7a4f51beeb
Tài Lê <letai97md@gmail.com>
2021-09-12 15:23:59 +0700

    creat card and data

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -1,60 +1,95 @@
-import './home.scss';
-import { Button, Icon } from "semantic-ui-react";
-import Navbar from '../../components/navbar/navbar'
+import "./home.scss";
+import React, { useState, useEffect } from "react";
+import Navbar from "../../components/navbar/navbar";
+import Card from "../../components/cards/card";
+import products from "../../assets/data/product";
+import { Icon, Input } from "semantic-ui-react";
 
 function Home() {
+  const [data, setData] = useState([]);
+  const [search, setSearch] = useState("");
+  const [price, setPrice] = useState("");
+  const [brand, setBrand] = useState("");
+
+  const onChangeSearch = (e) =>{
+    setSearch(e.target.value);
+    console.log(e.target.value);
+  }
+  // const onChangeBrand = (e) =>{
+  //   setBrand(e.target.value);
+  // }
+
+  const onSubmitSearch = () =>{
+    setData(
+      products.filter((item) => 
+      item?.name?.toLocaleLowerCase()?.includes(search?.toLocaleLowerCase())
+      )
+    );
+  };
+  const onSubmitBrand =async (e) => {
+    await setBrand(e.target.value);
+    await setData(
+      products.filter((item) => 
+      item?.name?.toLocaleLowerCase()?.includes(e.target.value?.toLocaleLowerCase())
+      )
+    )
+  }
+  const onSubmitPrice =async (e) => {
+    setPrice(e.target.value);
+    if(e.target.value == 1){
+      setData(
+        products.sort((a,b) => parseFloat(a.price) - parseFloat(b.price) )
+      )
+    }else{
+      setData(
+        products.sort((a,b) => parseFloat(b.price) - parseFloat(a.price) )
+      )
+    }
+  }
+
+  const fetchData = async () => {
+    await setData(products);
+  };
+
+  useEffect(async () => {
+    await fetchData();
+  }, []);
+
+
+
   return (
     <div className="home-container">
       <Navbar />
+
+      <div className="menuLeft">
+        <Input
+          icon={<Icon name="search" inverted circular link onClick = {onSubmitSearch} />}
+          placeholder="Search..." value={search} onChange = {onChangeSearch} 
+        />
+        <div className="selectForm">
+          <p>Hãng</p>
+          <select className="selectBox" value={brand} onChange = {onSubmitBrand}>
+            <option selected value="">All</option>
+            <option value="Asus">ASUS</option>
+            <option value="Dell">DELL</option>
+            <option value="Acer">ACER</option>
+            <option value="Lenovo">LENOVO</option>
+          </select>
+        </div>
+        <div className="selectForm">
+          <p>Giá</p>
+          <select className="selectBox" value={price} onChange ={onSubmitPrice}>
+            <option selected value=""></option>
+            <option value="1">Từ thấp đến cao</option>
+            <option value="2">Từ cao đến thấp</option>
+          </select>
+        </div>
+        
+      </div>
       <div className="product">
-        <div className="card-container" onClick={() => { }}>
-          <img className="image" src="https://phucanhcdn.com/media/product/35600_laptop_asus_a412fa_ek378t__blue__1_1.png" />
-          <h4 className="name">Asus A series A412FA-EK378T</h4>
-          <p className="email d-flex">Hãng: <span className="ml-1 text-success font-weight-bold">ASUS</span></p>
-          <p className="email d-flex">Chip xử lý: <span className="ml-1 text-success font-weight-bold">Core i7 10th</span></p>
-          {/* <button onClick={() => { }} className="btn-success">
-            Mua ngay
-          </button> */}
-          <Button onClick={() => {}} color="green">
-            <Icon name="eye" /> Xem sản phẩm
-          </Button>
-        </div>
-        <div className="card-container" onClick={() => { }}>
-          <img className="image" src="https://phucanhcdn.com/media/product/35600_laptop_asus_a412fa_ek378t__blue__1_1.png" />
-          <h4 className="name">Asus A series A412FA-EK378T</h4>
-          <p className="email d-flex">Hãng: <span className="ml-1 text-success font-weight-bold">ASUS</span></p>
-          <p className="email d-flex">Chip xử lý: <span className="ml-1 text-success font-weight-bold">Core i7 10th</span></p>
-          {/* <button onClick={() => { }} className="btn-success">
-            Mua ngay
-          </button> */}
-          <Button onClick={() => {}} color="green">
-            <Icon name="eye" /> Xem sản phẩm
-          </Button>
-        </div>
-        <div className="card-container" onClick={() => { }}>
-          <img className="image" src="https://phucanhcdn.com/media/product/35600_laptop_asus_a412fa_ek378t__blue__1_1.png" />
-          <h4 className="name">Asus A series A412FA-EK378T</h4>
-          <p className="email d-flex">Hãng: <span className="ml-1 text-success font-weight-bold">ASUS</span></p>
-          <p className="email d-flex">Chip xử lý: <span className="ml-1 text-success font-weight-bold">Core i7 10th</span></p>
-          {/* <button onClick={() => { }} className="btn-success">
-            Mua ngay
-          </button> */}
-          <Button onClick={() => {}} color="green">
-            <Icon name="eye" /> Xem sản phẩm
-          </Button>
-        </div>
-        <div className="card-container" onClick={() => { }}>
-          <img className="image" src="https://phucanhcdn.com/media/product/35600_laptop_asus_a412fa_ek378t__blue__1_1.png" />
-          <h4 className="name">Asus A series A412FA-EK378T</h4>
-          <p className="email d-flex">Hãng: <span className="ml-1 text-success font-weight-bold">ASUS</span></p>
-          <p className="email d-flex">Chip xử lý: <span className="ml-1 text-success font-weight-bold">Core i7 10th</span></p>
-          {/* <button onClick={() => { }} className="btn-success">
-            Mua ngay
-          </button> */}
-          <Button onClick={() => {}} color="green">
-            <Icon name="eye" /> Xem sản phẩm
-          </Button>
-        </div>
+        {data.map((item) => (
+          <Card product={item} />
+        ))}
       </div>
     </div>
   );

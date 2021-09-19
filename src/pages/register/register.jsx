46933474bda5d5c4e8b90285cce805585a945c7c
Tài Lê <letai97md@gmail.com>
2021-09-19 12:59:46 +0700

    creat login, register and notFuond

--- src/pages/register/register.jsx
+++ src/pages/register/register.jsx
@@ -2,7 +2,7 @@ import Navbar from "../../components/navbar/navbar";
 import "./register.scss";
 import { Input, Button } from "semantic-ui-react";
 import React, { useState } from "react";
-import {useHistory} from 'react-router-dom'
+import { useHistory } from "react-router-dom";
 
 const account = { username: "admin", password: "admin" };
 
@@ -10,28 +10,32 @@ function Register() {
   const [userName, setUserName] = useState("");
   const [password, setPassword] = useState("");
   const [cfpassword, setcfPassword] = useState("");
-  const history = useHistory()
+  const history = useHistory();
   const handleChange = (e, field) => {
-    if(field == 'username'){
-        setUserName(e.target.value);
-    }else if(field == 'password'){
-        setPassword(e.target.value)
-    }else if(field == 'cfpassword'){
-        setcfPassword(e.target.value)
+    if (field == "username") {
+      setUserName(e.target.value);
+    } else if (field == "password") {
+      setPassword(e.target.value);
+    } else if (field == "cfpassword") {
+      setcfPassword(e.target.value);
     }
   };
   const onLogin = () => {
     console.log(userName, password);
-    if(cfpassword === password){
-        console.log('Đăng ký thành công');
-        alert('Đăng ký thành công!!!')
-        history.push('/login')
-    }else{
-        console.log('Đăng ký thất bại');
-        alert('Mật khẩu không trùng nhau!!!')
-        setcfPassword('')
+    if (userName == "" || password == "" || cfpassword == "") {
+      alert("Yêu cầu nhập đủ thông tin!!");
+    } else {
+      if (cfpassword === password) {
+        console.log("Đăng ký thành công");
+        alert("Đăng ký thành công!!!");
+        history.push("/login");
+      } else {
+        console.log("Đăng ký thất bại");
+        alert("Mật khẩu không trùng nhau!!!");
+        setcfPassword("");
+      }
     }
-  }
+  };
   return (
     <div>
       <Navbar />
@@ -71,9 +75,9 @@ function Register() {
               value={cfpassword}
             />
             <br />
-            <Button color="green" onClick = {onLogin}> 
-             Đăng ký
-             </Button>
+            <Button color="green" onClick={onLogin}>
+              Đăng ký
+            </Button>
           </div>
         </div>
       </div>
